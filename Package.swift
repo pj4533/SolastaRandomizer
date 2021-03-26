75499ed5408064930e7741f9bfc074049f55339b
PJ Gray <pj@evertrue.com>
2021-03-26 17:31:08 -0400

    removed test target

--- Package.swift
+++ Package.swift
@@ -28,9 +28,5 @@ let package = Package(
             ]
         ),
         .target(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
     ]
 )
